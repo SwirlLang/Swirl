a5f21ce01047532630532214dc8ca999c936e8ab
In-diaonic <85999705+In-diaonic@users.noreply.github.com>
2021-09-14 22:54:18 +0530

    Create conventions.py

---
+++ core/conventions.py
@@ -0,0 +1,17 @@
+""" Class defining all naming conventions and the formatter for Lambda-code (still in development)"""
+
+
+class Coventions:
+  def apply_camel_case(self, snippet: str) -> None:
+    """ apply camel case convention to the given snippet of code
+    :param snippet: Any content yo apply the convention on
+    
+    Description: in Lambda-code camel case convention should be applied to all functions
+    """
+    
+  def apply_snake_case(self, snippet: str) -> None:
+    """ apply snake case convention to the given snippet 
+    :param snippet: Any content yo apply the convention on
+    
+    Description: in Lambda-code snake case conventions should be applied to all class variables (top-level vars can be written in any form)
+    """
