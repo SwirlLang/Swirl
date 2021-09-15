90585163e7fbd0f548f20f928362aa8f9fbffcba
mrinmoyhaloi <mrinmoyhaloi4@gmail.com>
2021-09-15 22:48:19 +0530

    some fixes
    Co-authored-by: DeveloperLuxo <DeveloperLuxo@users.noreply.github.com>

--- core/compiler.py
+++ core/compiler.py
@@ -6,21 +6,26 @@ import argparse
 parser = argparse.ArgumentParser(
     prog="compiler", description="Compiler for lambda code"
 )
-file_arg = parser.add_argument("file", type=str,)
+file_arg = parser.add_argument(
+    "file",
+    type=str,
+)
 parsed = parser.parse_args()
+
 
 class CompilerError(Exception):
     pass
 
+
 filename = ""
 
 filename = parsed.file
-print(filename)
 
 source = open(filename, "r")
 readed_file = source.read()
 functions = []
 index = readed_file.find("func")
+fi2 = readed_file.find("endfunc")
 comment_index = readed_file.find("///")
 gvariables = {}
 
@@ -28,15 +33,34 @@ while comment_index != -1:
     ci2 = readed_file.find("///", comment_index + 3)
     if ci2 == -1:
         row = readed_file[:comment_index].count("\n") + 1
-        col = 1 + comment_index - readed_file[:comment_index].rfind("\n")                     
+        col = comment_index + 1 - (readed_file[:comment_index].rfind("\n") + 1)
         raise CompilerError(f"{row}:{col}: error: unfinished multi-line comment")
     readed_file = readed_file.replace(readed_file[comment_index : ci2 + 3], "")
 
 while index != -1:
-    if readed_file.find("func", index + 4) == -1:
-        pass #gonna make this later
-    functions.append()
-    index = readed_file.find("func", index)
+    row = readed_file[:index].count("\n") + 1
+    col = index + 1 - (readed_file[:index].rfind("\n") + 1)
+    fi2 = readed_file.find("endfunc", index)
+    if index == fi2 - 3:
+        raise CompilerError(f"{row}:{col}: missing func to corresponding endfunc")
+
+    elif fi2 == -1:
+        raise CompilerError(f"{row}:{col}: unfinished function declaration")
+	
+    tmp = readed_file[index + 4 : fi2]
+    tmp = tmp.replace("\\\"", "")
+    tmp = tmp.replace("\\'", "")
+    string_index = tmp.find("\"")
+    si2 = tmp.find("\"", string_index + 1)
+    if si2 == -1:
+        raise CompilerError(f"{row}:{col} unfinished string inside this function")
+    if "func" in tmp or "class" in tmp:
+        raise CompilerError(
+            f"{row}:{col}: cannot declare a class or function inside another function, bruh"
+        )
+
+    functions.append(readed_file[index + 4 : fi2])
+    index = readed_file.find("func", fi2 + 7)
 
 main_func = readed_file
 
