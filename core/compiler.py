56d5a1652426b7c70f92439018a0f86225214f8d
In-diaonic <85999705+In-diaonic@users.noreply.github.com>
2021-09-19 02:23:19 +0530

    Add files via upload
    Several changes :)

--- core/compiler.py
+++ core/compiler.py
@@ -1,10 +1,15 @@
 """ The core of the compiler """
 
+import os
 import re
 import argparse
 import sys
+import pathlib
 
 sys.tracebacklimit = 0
+
+# contains the debug dir path of the current project in which exe's have to be produced
+env_debug_path_to_exe = None  # always acess this var after calling the compile function
 
 parser = argparse.ArgumentParser(
     prog="compiler", description="Compiler for lambda code"
@@ -17,7 +22,8 @@ parsed = parser.parse_args()
 
 
 class CompilerError(Exception):
-    __module__ = "builtins"
+    """Class for compiler's errors"""
+    __module__ = "builtins"  # Removes the annoying "__main__." before the error
 
 
 filename = parsed.file
@@ -33,15 +39,13 @@ gvariables = {}
 while index != -1:
     row = readed_file[:index].count("\n") + 1
     col = index + 1 - (readed_file[:index].rfind("\n") + 1)
-    fi2 = readed_file.find("endfunc", index)
+    fi2 = readed_file.find("endfunc", index - 3)
     if index == fi2 + 3:
         raise CompilerError(
-            f"Line {row}, column {col}: missing func to corresponding endfunc"
+            f"Line {row}, column {col - 3}: missing func to corresponding endfunc"
         )
 
     elif fi2 == -1:
-        print(index)
-        print(fi2)
         raise CompilerError(
             f"Line {row}, column {col}: unfinished function declaration"
         )
@@ -61,21 +65,49 @@ while index != -1:
         )
 
     nextfunc = readed_file[index + 4 : fi2].replace(" ", "")
+    nextfunc = nextfunc[: nextfunc.find(":")]
+    if nextfunc == "main":
+        raise CompilerError(f"Line {row}, column {col}: cannot call a function \"main\" because the main function is placed in the global scope")
+    
     for func in functions:
         func = func.replace(" ", "")
-        if nextfunc[: nextfunc.find(":")] == func[: func.find(":")]:
+        if nextfunc == func[: func.find(":")]:
             raise CompilerError(
                 f"Line {row}, column {col}: declaration of duplicate function"
             )
     functions.append(readed_file[index + 4 : fi2])
     index = readed_file.find("func", fi2 + 7)
 
-main_func = readed_file
+string_indices = []
+s_index1 = readed_file.find("'")
+s_index2 = readed_file.find('"')
 
-for func in functions:
-    main_func = main_func.replace(func, "")
+# you didn't specify the file
 
-print(functions)
+if s_index1 < s_index2:
+    row = readed_file[: s_index1].count("\n") + 1
+    if readed_file[s_index1 - 1 : s_index1 + 1] == "\\'":
+        col = s_index1 - (readed_file[: s_index1].rfind("\n") + 1)
+        raise CompilerError(f"Line {row}, column {col}: unexpected backslash")
+    singlei = readed_file.find("'", s_index1 + 1)
+    if singlei == -1:
+        col = s_index1 + 1 - (readed_file[: s_index1].rfind("\n") + 1)
+        raise CompilerError(f"Line {row}, column {col}: unfinished string")
+    while readed_file[singlei - 1 : singlei + 1] == "\\'" and readed_file[singlei - 2] != '\\':
+        singlei = readed_file.find("'", singlei + 1)
+    string_indices.append((s_index1, singlei))
+else:
+    if readed_file[s_index2 - 1 : s_index2 + 1] == '\\"':
+        row = readed_file[: s_index1].count("\n") + 1
+        col = s_index1 - (readed_file[: s_index1].rfind("\n") + 1)
+        raise CompilerError(f"Line {row}, column {col}: unexpected backslash")
+
+
+            
+# """Removes all functions, and what remains in the global scope will be the main code"""
+# for func in functions:
+#     main_func = main_func.replace(func, "")
+
 source.close()
 
 example_code = """
@@ -88,32 +120,59 @@ array arr[int,5] = (1,2,2,3,3,4)
 array arr[int,5] = 1, 2, 2, 3 , 3,4
 // hello world this is a comment
 """
+ 
+ 
+class RePattern:
+    """ Class defining patterns for LambdaCode (later used for pattern matching)"""
 
-
-class re_patterns:
     int_regex = "int \\w* = \\d"
     float_regex = "(float \\w* = \\w*\\.\\w*)|(float \\w* = \\w*)"
     string_regex = "string\[\d*\].*"
     array_regex = (
         "(array \\w*\\[\\w*,\\d\\] = \\d.*)|(array \\w*\\[\\w*,\\d\\] = \\(\\d.*)"
     )
+    class_regex = r"[a-z A-Z] [inherits]? [a-z A-Z]()\w.endclass{1}"  #TODO unstable, needs work 
 
 
-print(re.findall(re_patterns.int_regex, example_code))
+print(re.findall(RePattern.int_regex, example_code))
 print(
     [
         fnumber
-        for result in re.findall(re_patterns.float_regex, example_code)
+        for result in re.findall(RePattern.float_regex, example_code)
         for fnumber in result
         if fnumber != ""
     ]
 )
-print(re.findall(re_patterns.string_regex, example_code))
+# this is just for printing stuff and we'll remove them anyway okay :thumbsup:
+print(re.findall(RePattern.string_regex, example_code))
 print(
     [
         ar
-        for result in re.findall(re_patterns.array_regex, example_code)
+        for result in re.findall(RePattern.array_regex, example_code)
         for ar in result
         if ar != ""
     ]
 )
+
+# def precompile(snippet: str) -> str:
+#     """ Precompiles the code before translating it to CPP (e.g. removing comments and pasting imported stuffs) """
+#    # we can only ignore comments, not remove them, because we need the number of c
+
+def compile(data: str) -> str:
+    """ Compiles the snippet passed into the data param into C++ """
+    _home_dir = pathlib.Path.home()
+    try:
+        os.mkdir(f"{_home_dir}/debug")
+        os.mkdir(f"{_home_dir}/debug/{(parsed.file.split('.'))[0]}")
+        debug_dir = f"{_home_dir}/debug/{(parsed.file.split('.'))[0]}"
+        env_debug_path_to_exe = debug_dir
+    except Exception: 
+        pass
+    
+
+compile("ello ello")
+
+def debug():
+    """ Only meant for debugging purposes """
+    os.system(f"cd {pathlib.Path.home()}/Lambda-Code/core/")
+    os.system("python3 compiler.py print_statement.lc")
