be96b7cbbd31449a27b5571ee9afb9e6d6eaaca9
In-diaonic <indiaonic@gmail.com>
2022-01-14 16:46:13 +0530

    removed angular brackets from license's link

--- core/LCC.py
+++ core/LCC.py
@@ -19,7 +19,7 @@ Lambda Code is distributed in the hope that it will be useful, but WITHOUT ANY W
 warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License along with this program. If not,
-see <https://www.gnu.org/licenses/>. """
+see https://www.gnu.org/licenses/ """
 
 import os
 import sys
