18fdf148c91295fae75bc343515384612299ca5f
In-diaonic <indiaonic@gmail.com>
2022-01-26 21:32:27 +0530

    Added Complex numbers to namespace LambdaCode, also added their blueprint in the header file.

--- core/namespace_lc/lambda_code.h
+++ core/namespace_lc/lambda_code.h
@@ -1,5 +1,24 @@
 namespace __LambdaCode__ {
-    class string: public std::string {
-        
+    class Complex {
+
+    private:
+        long long re, im;
+
+    public:
+        Complex();
+
+        Complex(long long r, long long i);
+
+        Complex add(Complex other);
+
+        Complex sub(Complex other);
+
+        Complex mul(Complex other);
+
+        Complex div(Complex other);
+
+        long long real();
+
+        long long imaginary();
     };
 }
