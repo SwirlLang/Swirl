37519527f85a0489df4806a7c33a2db3a5997c53
In-diaonic <indiaonic@gmail.com>
2022-02-15 17:05:21 +0530

    String and Integer class, more methods coming soon

--- core/namespace_lc/string/String.cpp
+++ core/namespace_lc/string/String.cpp
@@ -1,76 +1,43 @@
 #include <iostream>
 #include <string>
-#include "string.h"
+#include <cstring>
+#include <optional>
+#include <vector>
 
 class String {
 private:
-    template<typename _Any>
-    bool _checkMembership(_Any member, _Any array) {
-        for (auto iter : array)
-        {
-            if (iter == *member) return true;
-            else
-                return false;
-        }
-    }
-
-    // template<typename _Any>
-    // int _getIndex(_Any member, _Any array) {
-    //     int loop_count = 0;
-
-    //     for (auto iter : array) {
-    //         if (loop_count != 0) {
-
-    //         }
-    //     }
-    // }
-
-public:
     std::string value;
 
-    String(std::string value): value(value){};
-    String();
+public:
+    String(std::string value): value(value) {}
+    String(): value(nullptr) {}
 
-    std::string upper() {
-        std::string ret = "";
-        std::string case_map[2][26] = {
-                {
-                    "a", "b", "c", "d", "e", "f", "g", "h",
-                    "i", "j", "k", "l", "m", "n", "o", "p",
-                    "q", "r", "s", "t", "u", "v", "w", "x",
-                    "y", "z"
-                    },
+    std::string& operator+(const std::string& value) { return this->value += value; }
+    
+    bool operator!=(const std::string& value) { return this->value != value; }
+    bool operator==(const std::string& value) { return this->value == value; }
 
-                    {
-                    "A", "B", "C", "D", "E", "F", "G", "H",
-                    "I", "J", "K", "L", "M", "N", "O", "P",
-                    "Q", "R", "S", "T", "U", "V", "W", "X",
-                    "Y", "Z"
-                    }
-        };
-
-        for (int chr_i = 0; chr_i < value.length(); chr_i++) {
-            if (_checkMembership((std::string*)value[chr_i], case_map[0])) {
-                // TODO
-            }
+    std::vector<std::string> split(std::string delitemer) {
+        std::vector<std::string> result;
+        int start = 0;
+        int end = 0;
+        while (end != -1) {
+            end = this->value.find(delitemer, start);
+            result.push_back(this->value.substr(start, end - start));
+            start = end + delitemer.length();
         }
+        return result;
+    }
 
-        return ret;
+    int length() { return this->value.length(); }
+    int find(std::string value) { return this->value.find(value); }
+    bool has(std::string value) { return this->value.find(value) != -1; }
+    bool isEmpty() { return this->value.length() == 0; } 
+
+    std::string& multiply(int value) {
+        for (int i = 0; i < value; i++) {
+            this->value += this->value;
+        }
+        return this->value;
     }
 };
-
-
-using std::cout;
-
-template<typename _Any>
-int _getIndex(_Any member = nullptr, _Any array = nullptr) {
-    int loop_count = 0;
-    auto count = []() {loop_count != 0 ? loop_count += 1 : return};
-    std::for_each(array.begin(), array.end(), count);
-}
-
-int main()
-{
-    std::string e[] = {"Hello World!"};
-    //_getIndex();
-}
