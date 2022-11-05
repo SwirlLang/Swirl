d357eabee5af084c5e6abf5431455a515bd32f94
indiaonic <indiaonic@gmail.com>
2022-11-05 22:54:37 +0530

    Added `var` to definitions

--- Swirl/include/definitions/definitions.h
+++ Swirl/include/definitions/definitions.h
@@ -9,15 +9,18 @@ struct defs {
         Parentheses, Operator, Brace, Punctuation
     };
 
-    std::array<const char *, 17> keywords = {
+    std::array<const char *, 20> keywords = {
             "func", "return", "if", "else", "for", "while",
             "is", "in", "or", "and", "class", "public",
-            "static", "int", "string", "float", "bool"
+            "static", "int", "string", "float", "bool", "true",
+            "false", "var"
     };
 
     std::array<std::string, 5> builtins = {
             "print", "len", "abs", "hash", "eval"
     };
+
+    std::array<char, 9> op_chars = {'*', '!', '=', '%', '+', '-', '/', '>', '<'};
 
     std::array<char, 6> delimiters = {'(', ')', ' ', '\n', ')', '{',};
 };
