8a3d77edd3b0b3e579fae64802f45cdf06839898
indiaonic <indiaonic@gmail.com>
2022-11-05 22:55:11 +0530

    Bug fixes in character input stream.

--- Swirl/include/tokenizer/InputStream.h
+++ Swirl/include/tokenizer/InputStream.h
@@ -1,15 +1,15 @@
 #include <iostream>
 #include <vector>
 
-#ifndef TOKENIZER_H_Swirl
-#define TOKENIZER_H_Swirl
+#ifndef INPUT_STREAM_H_Swirl
+#define INPUT_STREAM_H_Swirl
 
 
 class InputStream {
     std::string m_Source;
-    std::size_t m_Pos = 0, m_Line = 0, m_Col = 0;
+    std::size_t m_TotalLns = 0, m_LstChars = 0, m_Pos = 0, m_Line = 0, m_Col = 0;
 public:
-    explicit InputStream(std::string& _source): m_Source(_source) {}
+    explicit InputStream(std::string& _source);
 
     /** @brief Returns the next value without discarding it */
     char peek();
@@ -18,29 +18,14 @@ public:
     char next();
 
     /** @brief returns true if no more chars are left in the stream */
-    uint8_t eof();
+    bool eof();
 
     /** @brief raises an exception */
     void raiseException(const char* message);
+
+    std::size_t getPos() const;
+    std::size_t getLine() const;
+    std::size_t getCol() const;
 };
-
-
-char InputStream::peek() {
-    return m_Source.at(m_Pos);
-}
-
-char InputStream::next() {
-    char chr = m_Source.at(m_Pos++);
-    if (chr == '\n') m_Line++, m_Col = 0; else m_Col++;
-    return chr;
-}
-
-uint8_t InputStream::eof() {
-    return strlen(reinterpret_cast<const char *>(peek())) == 0;
-}
-
-void InputStream::raiseException(const char* message) {
-    // TODO
-}
 
 #endif
---
+++ Swirl/src/tokenizer/InputStream.cpp
@@ -0,0 +1,37 @@
+#include <sstream>
+#include <tokenizer/InputStream.h>
+
+InputStream::InputStream(std::string& _source): m_Source(_source) {
+    std::stringstream src_strm(_source);
+    for (std::string cr_ln; std::getline(src_strm, cr_ln); m_TotalLns++ ) {}
+    std::cout << m_TotalLns << std::endl;
+}
+
+char InputStream::peek() {
+    return m_Source.at(m_Pos);
+}
+
+char InputStream::next() {
+    char chr = m_Source.at(m_Pos++);
+    return chr;
+}
+
+bool InputStream::eof() {
+    return peek() == '\0';
+}
+
+void InputStream::raiseException(const char* message) {
+    // TODO
+}
+
+std::size_t InputStream::getPos() const {
+    return m_Pos;
+}
+
+std::size_t InputStream::getLine() const {
+    return m_Line;
+}
+
+std::size_t InputStream::getCol() const {
+    return m_Col;
+}
