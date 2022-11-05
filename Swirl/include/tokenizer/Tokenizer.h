ab9dce07fca53f008d6c0f1a8f4b9db42e5884be
indiaonic <indiaonic@gmail.com>
2022-11-05 22:56:02 +0530

    Bug and memory leak fixes in `Tokenizer`.

--- Swirl/include/tokenizer/Tokenizer.h
+++ Swirl/include/tokenizer/Tokenizer.h
@@ -6,34 +6,39 @@
 #include <cstring>
 #include <sstream>
 
+#include <definitions/definitions.h>
 #include <tokenizer/InputStream.h>
 #include <utils/utils.h>
+#include <map>
 
 #ifndef SWIRL_TokenStream_H
 #define SWIRL_TokenStream_H
 
 #define SWIRL_TokenStream_H
 
+const defs DEF{};
+
 class TokenStream {
     std::string m_Ret;
     std::string m_Rax;
     InputStream m_Stream;
-    std::array<const char*, 2> m_Cur = {nullptr, nullptr};
-    std::string keywords = "func return if else for while"
-                           " is in or and class public"
-                           " static int string float bool";
+    std::array<const char*, 2> m_PeekTk{};
+    std::array<const char*, 2> m_Cur = {};
 public:
+    std::array<const char*, 2> p_CurTk = {"", ""};
+
     explicit TokenStream(InputStream& _stream, bool _debug = false) : m_Stream(_stream) {}
 
-    bool isKeyword(const std::string& _str) { return keywords.find(_str) != std::string::npos; }
-    static bool isDigit(char _chr) { return std::string("1 2 3 4 5 6 7 8 9 0").find(std::string(1, _chr)) != std::string::npos; }
+    bool isKeyword(const std::string& _str) { return std::find(DEF.keywords.begin(), DEF.keywords.end(), _str) != DEF.keywords.end(); }
+
+    static bool isDigit(char _chr) { return std::string("1234567890").find(std::string(1, _chr)) != std::string::npos; }
     static uint8_t isId(char chr) { return isIdStart(chr) || std::string("\"?!-<>=0123456789").find(chr) != std::string::npos; }
     static bool isIdStart(char _chr) {
         return std::string("_abcdefghijklmnopqrstuvwxyz").find(std::string(1, _chr)) != std::string::npos;
     }
 
-    static uint8_t isPunctuation(char chr) { return std::string("();,{}[]").find(chr) >= 0; }
-    static bool isOpChar(char _chr) { return std::string("/=>*-%").find(_chr) != std::string::npos; }
+    static bool isPunctuation(char chr) { return std::string("();,{}[]").find(chr) >= 0; }
+    static bool isOpChar(char _chr) { return std::find(DEF.op_chars.begin(), DEF.op_chars.end(), _chr) != DEF.op_chars.end(); }
     static bool isWhiteSpace(char _chr) { return std::string(" \t\n").find(_chr) != std::string::npos; }
 
     std::string readWhile(const std::function<bool (char)>& delimiter) {
@@ -69,7 +74,7 @@ public:
         return {"STRING", m_Ret.c_str()};
     }
 
-    double parseDouble( const std::string& s ) {
+    static double parseDouble( const std::string& s ) {
         std::istringstream i(s);
         double x;
         if (!(i >> x)) { return std::numeric_limits<double>::quiet_NaN(); }
@@ -79,14 +84,14 @@ public:
     std::array<const char*, 2> readIdent() {
         m_Rax = readWhile(isId);
         return {
-                isKeyword(m_Rax) ? "ident" : "var",
+                isKeyword(m_Rax) ? "KEYWORD" : "IDENT",
                 m_Rax.c_str()
         };
     }
 
     std::array<const char*, 2> readNumber() {
         static uint8_t has_decim = false;
-        auto number = readWhile([this](char ch) -> uint8_t {
+        auto number = readWhile([](char ch) -> uint8_t {
             if (ch == '.') {
                 if (has_decim) return false;
                 has_decim = true;
@@ -95,7 +100,7 @@ public:
         });
 
         m_Ret = std::to_string(parseDouble(number));
-        return {"number", m_Ret.c_str()};
+        return {"NUMBER", m_Ret.c_str()};
     }
 
     std::array<const char*, 2> readNextTok() {
@@ -106,37 +111,38 @@ public:
         if (isIdStart(chr)) return readIdent();
 
         m_Ret = std::string(1, m_Stream.next());
-        if (isPunctuation(chr)) return {
-                    "punc",
+        if (isPunctuation(chr) ) return {
+                    "PUNC",
                     m_Ret.c_str()
             };
 
+        std::cout << chr;
         if (isOpChar(chr)) {
+            std::cout << "Is op char! " << chr << "\n";
             m_Rax = readWhile(isOpChar);
             return {
-                    "operator",
+                    "OPERATOR",
                     m_Rax.c_str()
             };
         }
 
-        m_Stream.raiseException(strcat("Failed to handle token ", &chr));
+        m_Stream.raiseException(strcat((char*)"Failed to handle token ", &chr));
     }
 
-    auto peek() {
-        if (m_Cur.empty()) {
-            m_Cur = readNextTok();
-            return m_Cur;
-        }
-    }
-
-    auto next() {
-        auto token = m_Cur;
-        m_Cur = {nullptr, nullptr};
-        if (token[0] && token[1] == nullptr)
-            return readNextTok();
+    std::array<const char*, 2> next() {
+        p_CurTk = readNextTok();
+        return p_CurTk;
     }
 
     bool eof() { return m_Stream.eof(); }
+
+    std::map<const char*, std::size_t> getStreamState() {
+        std::map<const char*, std::size_t> stream_state;
+        stream_state["LINE"] = m_Stream.getLine();
+        stream_state["POS"] = m_Stream.getPos();
+        stream_state["COL"] = m_Stream.getCol();
+        return stream_state;
+    }
 };
 
 #endif
