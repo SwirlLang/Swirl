f6603e96e9c5131b6279398f108a05c891700f29
indiaonic <indiaonic@gmail.com>
2022-11-10 16:59:17 +0530

    Fixed bugs with parsing calls.

--- Swirl/src/parser/parser.cpp
+++ Swirl/src/parser/parser.cpp
@@ -23,20 +23,23 @@ std::array<const char*, 2> Parser::next(std::array<const char *, 2>& _lvalue) {
 }
 
 void Parser::dispatch() {
-//    try {
-//        while (!m_Stream.eof()) {
-//            std::string t_val(m_Stream.p_CurTk[1]);
-//            std::string t_type(m_Stream.p_CurTk[0]);
-//            const char* tmp_ident;
-//
-//            if (t_type == "IDENT") {
-//                tmp_ident = m_Stream.p_CurTk[1];
-//                if (m_Stream.next() == std::array<const char*, 2> {"PUNC", "("}) {
-//                    parseCall(tmp_ident);
-//                }
-//            }
-//        }
-//    } catch ( std::exception& _sigbrt ) {}
+    std::string tmp_ident;
+    try {
+        while (!m_Stream.eof()) {
+            std::string t_val(m_Stream.p_CurTk[1]);
+            std::string t_type(m_Stream.p_CurTk[0]);
+            if (t_type == "IDENT") {
+                tmp_ident = t_val;
+                m_Stream.next();
+                std::cout << std::string(m_Stream.p_CurTk[1]);
+                if (std::string(m_Stream.p_CurTk[1]) == "(" ) {
+                    std::cout << m_Stream.p_CurTk[1] << m_Stream.p_CurTk[0] << std::endl;
+                    parseCall(tmp_ident.c_str());
+                }
+            }
+            m_Stream.next();
+        }
+    } catch ( std::exception& _sigbrt ) {}
 }
 
 void Parser::parseDecl() {
@@ -53,13 +56,13 @@ void Parser::parseDecl() {
             std::string decl_val;
             if (m_Stream.p_CurTk[0] == "NUMBER" || m_Stream.p_CurTk[0] == "STRING") {
                 decl_val = m_Stream.p_CurTk[1];
-                decl_node.value = m_Stream.p_CurTk;
+//                decl_node.value = m_Stream.p_CurTk;
                 break;
             }
             m_Stream.next();
         }
     } catch ( std::exception& _sigbrt ) { std::cout << _sigbrt.what(); }
-    decl_node.value = m_Stream.p_CurTk;
+//    decl_node.value = m_Stream.p_CurTk;
     if (!m_AppendToScope) m_AST->chl.push_back(decl_node);
 }
 
@@ -67,7 +70,7 @@ void Parser::parseCall(const char* _ident) {
     Node call_node{};
     Node arg_node{};
     call_node.type = "CALL";
-//    call_node.value = _ident, call_node.ident = _ident;
+    call_node.ident = _ident;
 
     while (strcmp(m_Stream.next()[1], ")") != 0) {
         const auto tk_cache = m_Stream.p_CurTk;
@@ -75,38 +78,35 @@ void Parser::parseCall(const char* _ident) {
         const std::string tk_val(tk_cache[1]);
 
 //        std::cout << m_Stream.p_CurTk[0] << std::endl;
-        if (tk_type != "PUNC" && tk_val != _ident) {
+        if (tk_type != "PUNC") {  // TODO: scenario not considered: f(<object>.<method>())
 //            std::cout << "REACHED!" << std::endl;
             arg_node.type = tk_type;
-//            arg_node.value = tk_val;
+            arg_node.value = tk_val;
+            std::cout << arg_node.value[0] << arg_node.value[1];
             call_node.args.push_back(arg_node);
         }
     }
+
     if (!m_AppendToScope) m_AST->chl.push_back(call_node);
 }
 
 void Parser::parseCondition() {
-    const char *cmp1 = nullptr;
-    const char *cmp_opr = nullptr;
-    const char *cmp2 = nullptr;
-
     Node cnd_node{};
     cnd_node.type = "CONDITION";
-    std::array<const char *, 4> valid_cmp_tokens = { "IDENT", "NUMBER", "BOOL", "STRING" };
+    std::string t_type(m_Stream.p_CurTk[0]);
+    std::string t_val(m_Stream.p_CurTk[1]);
+    std::array<const char *, 4> valid_cmp_tk_type = { "IDENT", "NUMBER", "BOOL", "STRING" };
 
-    bool n_end_scp;
-    std::size_t cnd_parse_index = 0;
-    std::array<const char *, 2> cur_tok = m_Stream.next();
-    next(cur_tok);
+    try {
+        while (!m_Stream.eof()) {
+            if (std::find(valid_cmp_tk_type.begin(), valid_cmp_tk_type.end(), m_Stream.p_CurTk[0]) != valid_cmp_tk_type.end()) {
+                std::cout << "comparisons done in: " << m_Stream.p_CurTk[1] << std::endl;
+            }
 
-    while (strcmp(cur_tok[1], "{") != 0) {
-        if (strcmp(cur_tok[1], " ") != 0) {
-            if (strcmp(cur_tok[0], "IDENT") != 0) {
-//                if (m_Stream.peekChar() == '(')
-//                    parseCall(cur_tok[1]);
+            m_Stream.next();
+            if (strcmp(m_Stream.p_CurTk[0], "PUNC") != 0) {
+                std::cout << "op: " << m_Stream.p_CurTk[1] << std::endl;
             }
         }
-        cur_tok = m_Stream.next();
-    }
-    std::cout << cmp1 << std::endl;
+    } catch ( std::exception& _sigbrt ) { }
 }
