e33169a5e0d08a9b77610714ef1c9d4b74233048
indiaonic <indiaonic@gmail.com>
2022-11-12 22:04:28 +0530

    Compiler checks for the existence of the file.

--- Swirl/src/swirl.cpp
+++ Swirl/src/swirl.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <fstream>
 #include <vector>
+#include <filesystem>
  
 #include <pre-processor/pre-processor.h>
 #include <swirl.typedefs/swirl_t.h>
@@ -32,7 +33,6 @@ Use swirl [command] --help for more information about the command)";
 
 
 int main(int argc, const char *argv[]) {
-    uint8_t pre_processor_exit_code;
     defs defs{};
 
     if (argc <= 1)
@@ -40,14 +40,19 @@ int main(int argc, const char *argv[]) {
 
     if (argc > 1) {
         swirl_FED_FILE_PATH = argv[1];
+        if (!std::filesystem::exists(swirl_FED_FILE_PATH)) {
+            std::cerr << "File " << swirl_FED_FILE_PATH << " doesn't exists!" << std::endl;
+            return 1;
+        }
         std::ifstream fed_file_src_buf(swirl_FED_FILE_PATH);
         std::string src_current_ln;
         while (std::getline(fed_file_src_buf, src_current_ln)){
             swirl_FED_FILE_SOURCE += src_current_ln + "\n";
         }
 
+        std::string cache_dir = getWorkingDirectory(swirl_FED_FILE_PATH) + PATH_SEP + "__swirl_cache__" + PATH_SEP;
         uint8_t _debug = strcmp(argv[0], "--debug") != 0;
-        preProcess(swirl_FED_FILE_SOURCE, swirl_FED_FILE_PATH, pre_processor_exit_code);
+        preProcess(swirl_FED_FILE_SOURCE, cache_dir);
         InputStream is(swirl_FED_FILE_SOURCE);
         TokenStream tk(is);
 //        while (!tk.eof()) {
@@ -65,9 +70,9 @@ int main(int argc, const char *argv[]) {
         std::string out_dir = swirl_FED_FILE_PATH.replace(swirl_FED_FILE_PATH.find(file_name),file_name.length(),"");
         file_name = file_name.substr(0, file_name.find_last_of("."));
 
-        Transpile(*parser.m_AST, getWorkingDirectory(swirl_FED_FILE_PATH) + PATH_SEP + "__swirl_cache__" + PATH_SEP + file_name +".cpp");
+        Transpile(*parser.m_AST, cache_dir + file_name +".cpp");
 
-        std::string cpp_obj = "g++ " + getWorkingDirectory(swirl_FED_FILE_PATH) + PATH_SEP + "__swirl_cache__" + PATH_SEP + file_name +".cpp" + " -o " + out_dir + file_name + " && " + "." + PATH_SEP + out_dir + file_name;
+        std::string cpp_obj = "g++ " + cache_dir + file_name +".cpp" + " -o " + out_dir + file_name + " && " + "." + PATH_SEP + out_dir + file_name;
         system(cpp_obj.c_str());
     }
 }
