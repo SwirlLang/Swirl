a723afb2fac8602704179e2b1e333f95cb3b33a6
indiaonic <indiaonic@gmail.com>
2022-11-13 22:41:37 +0530

    support for `--run`, `-d`, `--debug`

--- Swirl/src/swirl.cpp
+++ Swirl/src/swirl.cpp
@@ -32,13 +32,16 @@ Flags:
 Use swirl [command] --help for more information about the command)";
 
 
-int main(int argc, const char *argv[]) {
+int main(int argc, const char* argv[]) {
     defs defs{};
-
-    if (argc <= 1)
-        std::cout << swirl_help << std::endl;
-
+    std::vector<std::string> args(argv, argv + argc);
+    if (argc <= 1) { std::cout << swirl_help << std::endl;}
     if (argc > 1) {
+        if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+            std::cout << swirl_help << std::endl;
+            return 0;
+        }
+
         swirl_FED_FILE_PATH = argv[1];
         if (!std::filesystem::exists(swirl_FED_FILE_PATH)) {
             std::cerr << "File " << swirl_FED_FILE_PATH << " doesn't exists!" << std::endl;
@@ -51,16 +54,16 @@ int main(int argc, const char *argv[]) {
         }
 
         std::string cache_dir = getWorkingDirectory(swirl_FED_FILE_PATH) + PATH_SEP + "__swirl_cache__" + PATH_SEP;
-        uint8_t _debug = strcmp(argv[0], "--debug") != 0;
+        bool _debug;
+        if (std::find(args.begin(), args.end(), "-d") != args.end() ||
+            std::find(args.begin(), args.end(), "--debug") != args.end())
+            _debug = true;
+
         preProcess(swirl_FED_FILE_SOURCE, cache_dir);
         InputStream is(swirl_FED_FILE_SOURCE);
-        TokenStream tk(is);
-//        while (!tk.eof()) {
-//            auto d_tok = tk.next();
-//            std::cout << "TYPE: \t\t" << d_tok[0] << "\t VAL: \t\t" << d_tok[1] << std::endl;
-//        }
+        TokenStream tk(is, _debug);
+
         Parser parser(tk);
-//        parser.dispatch();
         parser.dispatch();
 //        for ( auto const& chl : parser.m_AST->chl) {
 //            std::cout << chl.type << std::endl;
@@ -70,10 +73,17 @@ int main(int argc, const char *argv[]) {
         std::string out_dir = swirl_FED_FILE_PATH.replace(swirl_FED_FILE_PATH.find(file_name),file_name.length(),"");
         file_name = file_name.substr(0, file_name.find_last_of("."));
 
-        Transpile(*parser.m_AST, cache_dir + file_name +".cpp");
+//        if (swirl_OUTPUT.empty()) swirl_OUTPUT = file_name;
+        Transpile(*parser.m_AST, cache_dir + file_name + ".cpp");
 
-        std::string cpp_obj = "g++ " + cache_dir + file_name +".cpp" + " -o " + out_dir + file_name + " && " + "." + PATH_SEP + out_dir + file_name;
-        system(cpp_obj.c_str());
+        if (std::find(args.begin(), args.end(), "--run") != args.end()) {
+            std::cout << "found";
+            std::string cpp_obj =
+                    "g++ " + cache_dir + file_name + ".cpp" + " -o " + out_dir + file_name + " && " + "." + PATH_SEP +
+                    out_dir + file_name;
+
+            system(cpp_obj.c_str());
+        }
     }
 }
 
