397b5065391f8452a8133a9fb941a1a63cda8db7
EMPTY <testerthe60@gmail.com>
2022-11-27 12:12:16 +0100

    Linux Installer: Added dependency version check

--- install.sh
+++ install.sh
@@ -1,14 +1,27 @@
 #!/usr/bin/env bash
 
-declare -a requirements=("gcc" "cmake")
+## https://stackoverflow.com/questions/4023830/how-to-compare-two-strings-in-dot-separated-version-format-in-bash
+checkVer() {
+    [  "$1" = "`echo -e "$1\n$2" | sort -V | head -n1`" ]
+}
 
-for req in ${requirements[@]};do
-  hash $req 2>/dev/null || { echo >&2 "[ERROR]: ${req} was not found in path, make sure it is installed correctly"; exit 1; }
+declare -a requirements=("gcc" "cmake")
+declare -a versions=( 6.1.0 3.5.0)
+
+for i in ${!requirements[@]};do
+  hash ${requirements[$i]} 2>/dev/null || { echo >&2 "[ERROR]: ${requirements[$i]} was not found in path, make sure it is installed correctly"; exit 1; }
+  matches=$(${requirements[$i]} --version | grep -E --only-match  "([0-9]+\.?)+")
+  #echo ${matches}
+  matchesArray=($(echo $matches | tr " " "\n"))
+  installedVer=${matchesArray[0]}
+
+  checkVer ${versions[$i]} $installedVer || { echo >&2 "[ERROR]: Your ${requirements[$i]} (${installedVer}) is outdated please use version ${versions[$i]} or higher"; exit 1; }
+    
 done
 
 echo "[INFO]: All dependencies are met!"
 cmake -B build -DCMAKE_BUILD_TYPE=Debug -S Swirl &>/dev/null
 echo "[INFO]: Building Swirl"
-cmake --build build --config Debug >/dev/null
+cmake --build build --config Debug &>/dev/null
 echo "Swirl is now available at ${pwd}/Swirl/build/swirl"
 ./build/swirl -h
