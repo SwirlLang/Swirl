9592e5fa1014fc0403bbe4af019536563503d23b
indiaonic <indiaonic@gmail.com>
2022-11-30 19:03:08 +0530

    defined logical operator macros.

--- Swirl/src/transpiler/builtins.cpp
+++ Swirl/src/transpiler/builtins.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 
+#define and &&
+#define not !
+#define or ||
+
 template < typename Const >
 void print(Const __Obj, const std::string& __End = "\n", bool __Flush = true) {
     if (__Flush) std::cout << __Obj << __End << std::flush;
@@ -10,5 +14,6 @@ std::string input(std::string __Prompt) {
     std::string ret;
     std::cout << __Prompt << std::flush;
     std::getline(std::cin, ret);
+
     return ret;
 }
