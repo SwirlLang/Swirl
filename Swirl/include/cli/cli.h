369473a110c63de4b858f927a6c37441ce02cabd
Pranjal Patel <pranjalpatel12@outlook.com>
2022-12-25 14:45:47 +0530

    Separated cli related stuff

---
+++ Swirl/include/cli/cli.h
@@ -0,0 +1,40 @@
+#ifndef SWIRL_CLI_H
+#define SWIRL_CLI_H
+
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <tuple>
+#include <variant>
+
+namespace cli {
+    struct Argument {
+        std::tuple<std::string, std::string> flags;
+        std::string desc;
+
+        bool value_required;
+        std::string value;
+    };	
+
+	#pragma region HELP MESSAGE
+	constexpr std::string_view HELP_MESSAGE = R"(The Swirl compiler
+
+	Usage: Swirl <file-path> [-o] <output> [--debug]
+
+	Flags:
+		-h, --help      Show this help message
+
+		-d, --debug     log out steps of the compilation
+		-o, --output    Output file name
+		-r, --run       Run the compiled file
+		-c, --compiler  C++ compiler to use
+	)";
+	//Use swirl [command] --help for more information about the command
+	#pragma endregion
+
+	std::vector<Argument> parse(int argc, const char** argv, const std::vector<Argument>& flags);
+    std::variant</* The flag value */ std::string, /* The flag is supplied or not */ bool>
+        get_flag(std::string_view flag, const std::vector<Argument>& args);
+}
+
+#endif // !SWIRL_CLI_H
---
+++ Swirl/src/cli/cli.cpp
@@ -0,0 +1,52 @@
+#include "cli/cli.h"
+
+namespace cli {
+	std::vector<Argument> parse(int argc, const char** argv, const std::vector<Argument>& flags) {
+		if (argc <= 1) { std::cout << HELP_MESSAGE; exit(0); }
+
+		std::vector<std::string_view> args(argv, argv + argc);
+		std::vector<Argument> supplied;
+
+		for (auto arg_iterator = args.cbegin() + 1; arg_iterator != args.cend(); ++arg_iterator) {
+			// if the current argument starts with `-` sign, its a flag
+			if (arg_iterator->starts_with("-")) {
+
+				// check if the flag exists in the flag vector
+				auto it = std::find_if(flags.cbegin(), flags.cend(), [&](const Argument& a) {
+					auto& [v1, v2] = a.flags;
+					return v1 == *arg_iterator || v2 == *arg_iterator;
+					});
+
+				if (it == flags.cend()) { std::cerr << "Unknown flag: " << *arg_iterator << '\n'; exit(1); }
+
+				if (!it->value_required) supplied.push_back(*it);
+				else {
+					if (arg_iterator + 1 == args.cend()) { std::cout << "Value missing for the flag: " << *arg_iterator << '\n'; exit(1); }
+
+					Argument _arg = *it;
+					_arg.value = *(arg_iterator + 1);
+					supplied.push_back(_arg);
+				}
+
+			}
+		}
+
+		return supplied;
+	}
+
+    std::variant</* The flag value */ std::string, /* The flag is supplied or not */ bool>
+		get_flag(std::string_view flag, const std::vector<Argument>& args) {
+		auto it = std::find_if(args.cbegin(), args.cend(), [&](const Argument& a) {
+			auto& [v1, v2] = a.flags;
+			return v1 == flag || v2 == flag;
+		});
+
+		// Flag not found
+		if (it == args.cend()) return false;
+		
+		// Flag exists
+		if (!it->value_required) return true;
+
+		return it->value;
+	}
+}
