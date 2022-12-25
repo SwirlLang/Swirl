03c6b1ff821c1f872d987e14014208d573b46f92
Pranjal Patel <pranjalpatel12@outlook.com>
2022-12-25 14:46:08 +0530

    Commented the code

--- Swirl/src/swirl.map/Map.c
+++ Swirl/src/swirl.map/Map.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+// Commend this because it was causing a problem with the visual studio build
+#if 0
 typedef struct {
 } HashTableItem;
 
 typedef struct {
 
 } HashTable;
+#endif
