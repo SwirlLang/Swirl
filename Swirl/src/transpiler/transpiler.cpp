26a7cc0d3fd65d1060152a7f8ddf6dbdb2f91162
indiaonic <indiaonic@gmail.com>
2022-12-27 23:17:44 +0530

    Transpiler: Built-ins are no longer read externally.

--- Swirl/src/transpiler/transpiler.cpp
+++ Swirl/src/transpiler/transpiler.cpp
@@ -1,37 +1,68 @@
 #include <iostream>
 #include <exception>
-#include <memory>
-#include <map>
-#include <fstream>
 
 #include <parser/parser.h>
-#include <definitions/definitions.h>
 
-#define _debug true
+std::string compiled_source = R"(
+#include <iostream>
 
+#define elif else if
+#define var auto
+
+template < typename Const >
+void print(Const __Obj, const std::string& __End = "\n", bool __Flush = true) {
+    if (__Flush) std::cout << std::boolalpha << __Obj << __End << std::flush;
+    else std::cout << std::boolalpha << __Obj << __End;
+}
+
+std::string input(std::string __Prompt) {
+    std::string ret;
+    std::cout << __Prompt << std::flush;
+    std::getline(std::cin, ret);
+
+    return ret;
+}
+)";
 
 void Transpile(AbstractSyntaxTree& _ast, const std::string& _buildFile) {
     bool                       is_scp;
+    bool                       apnd_chl;
     std::ifstream              bt_fstream;
     std::string                tmp_str_cnst;
     std::size_t                last_scp_order;
-    std::string                compiled_source;
-    std::array<const char*, 3> vld_scopes = {"CONDITION", "FUNC", "CLASS"};
-
-    bt_fstream.open("Swirl/src/transpiler/builtins.cpp");
-    compiled_source = {
-        std::istreambuf_iterator<char>(bt_fstream),
-        {}
-    };
-    bt_fstream.close();
 
     compiled_source += "int main() {\n";
 
     for (auto const& child : _ast.chl) {
+        if (child.type == "OP") {
+            compiled_source.erase(compiled_source.size() - 1);
+            compiled_source += child.value;
+
+            if (child.value == "++" || child.value == "--")
+                compiled_source += ";";
+            continue;
+        }
+
+        if (child.type == "STRING") {
+            compiled_source += '"' + child.value + "\";";
+            continue;
+        }
+
+        if (child.type == "NUMBER") {
+            compiled_source += child.value + ";";
+            continue;
+        }
+
+        if (child.type == "IDENT") {
+            compiled_source += child.value + ";";
+            continue;
+        }
+
         if (child.type == "BR_OPEN") {
             compiled_source += "{";
             continue;
         }
+
         if (child.type == "BR_CLOSE") {
             compiled_source += "}";
             continue;
@@ -47,7 +78,18 @@ void Transpile(AbstractSyntaxTree& _ast, const std::string& _buildFile) {
             continue;
         }
 
-        else if (child.type == "CALL") {
+        if (child.type == "VAR") {
+            compiled_source += child.var_type + " " + child.ident;
+            if (!child.initialized)
+                compiled_source += ";";
+            else {
+                apnd_chl = true;
+                compiled_source += "=";
+            }
+            continue;
+        }
+
+        if (child.type == "CALL") {
             tmp_str_cnst += child.ident + "(";
             int args_count = 0;
 
