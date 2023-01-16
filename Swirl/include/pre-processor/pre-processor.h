c37acbe2b2f5b32a3a7c4c5c4c64b3a3753404f5
indiaonic <indiaonic@gmail.com>
2023-01-16 20:53:20 +0530

    Core: Fix build failure.

--- Swirl/include/pre-processor/pre-processor.h
+++ Swirl/include/pre-processor/pre-processor.h
@@ -1,10 +1,8 @@
 #include <iostream>
-#include <sstream>
-#include <fstream>
-#include <vector>
-#include <utils/utils.h>
+
+#include <tokenizer/Tokenizer.h>
 
 #ifndef PRE_PROCESSOR_H_SWIRL
 #define PRE_PROCESSOR_H_SWIRL
-void preProcess(const std::string&, std::string);
+void preProcess(const std::string&, TokenStream&, std::string);
 #endif
--- Swirl/src/pre-processor/pre-processor.cpp
+++ Swirl/src/pre-processor/pre-processor.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
-#include <sstream>
 #include <fstream>
 #include <vector>
 #include <filesystem>
 
-#include <utils/utils.h>
-
+#include <tokenizer/Tokenizer.h>
 
 #ifndef _WIN32
 #define TORNADO_PKGS_PATH "/.tornado/packages/"
 #endif
 
-void preProcess(const std::string& _source, std::string _buildPath) {
+void preProcess(const std::string& _source, TokenStream& _stream, std::string _buildPath) {
     std::stringstream source_strm(_source);
-    std::vector<std::string> imp_paths;
+    std::vector<std::string> cimports{};
 
     std::string cr_dir = "cd " + _buildPath.erase(_buildPath.size() - 16, _buildPath.size())
             + "&& mkdir __swirl_cache__";
@@ -22,10 +20,4 @@ void preProcess(const std::string& _source, std::string _buildPath) {
         system(cr_dir.c_str());
 
     std::ofstream cache_file(_buildPath + "__swirl_cache__" + PATH_SEP + "__main__.sw");
-//
-//    for ( std::string cr_ln; std::getline(source_strm, cr_ln); ) {
-//        if (cr_ln.find("import") != std::string::npos && cr_ln.find("//") == std::string::npos) {
-//
-//        }
-//    }
 }
