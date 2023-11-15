daf2e93ed3ec00741dd9a419f0042bb23718400e
in-diaonic <indiaonic@gmail.com>
2023-11-16 01:36:50 +0530

    fix(backend): incorrect expression evaluation

--- Swirl/src/transpiler/transpiler.cpp
+++ Swirl/src/transpiler/transpiler.cpp
@@ -45,30 +45,45 @@ llvm::Value* Expression::codegen() {
                     a = Builder.CreateSIToFP(a, llvm::Type::getFloatTy(Context));
                 if (b->getType()->isIntegerTy()) {
                     b = Builder.CreateSIToFP(b, llvm::Type::getFloatTy(Context));
-                return Builder.CreateFDiv(a, b);
                 }
-            }
+                 return Builder.CreateFDiv(a, b);
+             }
             }
     };
 
     for (const std::unique_ptr<Node>& nd : expr) {
-        if (nd->getType() != ND_OP)
+        if (nd->getType() != ND_OP) {
+            std::cout << nd->getValue() << std::endl;
             eval.push(nd->codegen());
-        else if (nd->getType() == ND_OP) {
+            continue;
+        }
+        else {
             if (nd->getArity() == 2) {
+                std::cout << nd->getValue() << std::endl;
                 if (eval.size() < 2) { throw std::runtime_error("Not enough operands on eval stack"); }
-                llvm::Value* op1 = eval.top();
-                eval.pop();
                 llvm::Value* op2 = eval.top();
                 eval.pop();
-
+                llvm::Value* op1 = eval.top();
+                eval.pop();
                 eval.push(op_table[nd->getValue()](op1, op2));
             }
+            continue;
         }
     }
+
+    std::cout << "expr::codegen called" << ": " << std::endl;
+    std::cout << "size of eval stack: " << eval.size() << std::endl;
+    eval.top()->print(llvm::outs());
+    return eval.top();
 }
 
 llvm::Value* FuncCall::codegen() {
+    llvm::Type* returnType = llvm::Type::getVoidTy(Context);
+    std::vector<llvm::Type*> paramTypes;  // Empty parameter list for this example
+    llvm::FunctionType* functionType = llvm::FunctionType::get(returnType, paramTypes, false);
+    llvm::Function* myFunction = llvm::Function::Create(functionType, llvm::GlobalValue::ExternalLinkage, "print", LModule.get());
+
+
     llvm::Function* func = LModule->getFunction(ident);
     if (!func)
         throw std::runtime_error("No such function defined");
