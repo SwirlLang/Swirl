ec9cb95b8ca1a4ad2fee9a9ffb97bedb74fb3632
nullptr <indiaonic@gmail.com>
2025-06-09 14:50:55 +0530

    fix: keyword `let` unregistered in the definitions

--- compiler/include/definitions.h
+++ compiler/include/definitions.h
@@ -23,7 +23,7 @@ inline const std::unordered_map<std::string, uint8_t> keywords = {
     {"or", 8},        {"and", 9},     {"class", 10},  {"public", 11},
     {"private", 12},  {"const", 15},  {"static", 16}, {"break", 17},
     {"continue", 18}, {"elif", 19},   {"extern", 20}, {"importc", 21},
-    {"typedef", 22},  {"import", 23}, {"export", 24}, {"from", 25},
+    {"let", 22},      {"import", 23}, {"export", 24}, {"from", 25},
     {"var", 26},      {"fn", 27}, {"volatile", 28}, {"struct", 29}
 };
 
