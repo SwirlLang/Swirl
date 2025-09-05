88a6f2eeff5ed51c12ecc78cdbd7e338f5bd2d58
MrinmoyHaloi <69071143+MrinmoyHaloi@users.noreply.github.com>
2025-09-05 23:57:53 +0530

    style(cli): fix indentation and remove whitespace

--- compiler/src/cli/cli.cpp
+++ compiler/src/cli/cli.cpp
@@ -2,11 +2,11 @@
 #include <algorithm>
 
 cli::cli(int argc, const char** argv, const std::vector<Argument>& flags):
-	m_argc(argc),
-	m_argv(argv),
-	m_flags(&flags),
-	m_input_file(),
-  m_args(parse()) { }
+    m_argc(argc),
+    m_argv(argv),
+    m_flags(&flags),
+    m_input_file(),
+    m_args(parse()) { }
 
 bool cli::contains_flag(std::string_view flag) {
     return std::any_of(m_args.cbegin(), m_args.cend(), [&](const Argument& a) {
@@ -39,45 +39,45 @@ std::vector<std::string> cli::get_flag_values(std::string_view flag) {
 }
 
 std::string cli::generate_help() {
-	std::size_t max_width = 0;
-	std::for_each(m_flags -> cbegin(), m_flags -> cend(), [&](const Argument& arg) {
-		auto& [f1, f2] = arg.flags;
-		if (f1.size() + f2.size() + 2 > max_width) max_width = f1.size() + f2.size() + 3;
-	});
+    std::size_t max_width = 0;
+    std::for_each(m_flags->cbegin(), m_flags->cend(), [&](const Argument& arg) {
+        auto& [f1, f2] = arg.flags;
+        if (f1.size() + f2.size() + 2 > max_width) max_width = f1.size() + f2.size() + 3;
+    });
 
-	std::string msg;
-	for (const Argument& arg : *m_flags) {
-		auto& [f1, f2] = arg.flags;
-		msg += "\t" + f1 + ", " + f2;
-		for (unsigned int c = 0; c < max_width - f1.size() - f2.size() - 2; c++) msg += ' ';
-		msg += "     " + arg.desc + '\n';
-	} return msg;
+    std::string msg;
+    for (const Argument& arg : *m_flags) {
+        auto& [f1, f2] = arg.flags;
+        msg += "\t" + f1 + ", " + f2;
+        for (unsigned int c = 0; c < max_width - f1.size() - f2.size() - 2; c++) msg += ' ';
+        msg += "     " + arg.desc + '\n';
+    } return msg;
 }
 
 std::optional<std::string> cli::get_file() {
-	return m_input_file;
+    return m_input_file;
 }
 
 std::vector<Argument> cli::parse() {
-	if (m_argc <= 1) { 
-		std::cout << USAGE << generate_help() << '\n';
-		exit(0); 
-	}
+    if (m_argc <= 1) { 
+        std::cout << USAGE << generate_help() << '\n';
+        exit(0); 
+    }
 
-	std::vector<std::string_view> args(m_argv, m_argv + m_argc);
-	std::vector<Argument> supplied;
+    std::vector<std::string_view> args(m_argv, m_argv + m_argc);
+    std::vector<Argument> supplied;
 
-	for (auto arg_iterator = args.cbegin() + 1; arg_iterator != args.cend(); ++arg_iterator) {
-		// if the current argument starts with `-` sign, its a flag
-		if (arg_iterator->starts_with("-")) {
+    for (auto arg_iterator = args.cbegin() + 1; arg_iterator != args.cend(); ++arg_iterator) {
+        // if the current argument starts with `-` sign, its a flag
+        if (arg_iterator->starts_with("-")) {
 
             // check if the flag exists in the flag vector
-            auto flag_it = std::find_if(m_flags -> cbegin(), m_flags -> cend(), [&](const Argument& a) {
-				auto& [f1, f2] = a.flags;
-				return f1 == *arg_iterator || f2 == *arg_iterator;
+            auto flag_it = std::find_if(m_flags->cbegin(), m_flags->cend(), [&](const Argument& a) {
+                auto& [f1, f2] = a.flags;
+                return f1 == *arg_iterator || f2 == *arg_iterator;
             });
 
-            if (flag_it == m_flags -> cend()) { std::cerr << "Unknown flag: " << *arg_iterator << '\n'; exit(1); }
+            if (flag_it == m_flags->cend()) { std::cerr << "Unknown flag: " << *arg_iterator << '\n'; exit(1); }
 
             // Check if this flag was already supplied
             auto supplied_it = std::find_if(supplied.begin(), supplied.end(), [&](const Argument& a) {
@@ -120,5 +120,5 @@ std::vector<Argument> cli::parse() {
         }
     }
 
-	return supplied;
+    return supplied;
 }
