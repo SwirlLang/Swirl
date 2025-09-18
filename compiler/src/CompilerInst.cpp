dc970e2aa199065bb53e43fd1206ccfce00961cc
Mrinmoy Haloi <69071143+MrinmoyHaloi@users.noreply.github.com>
2025-09-18 22:51:38 +0530

    fix: windows linking (#44)
    Co-authored-by: nullptr <indiaonic@gmail.com>

--- compiler/src/CompilerInst.cpp
+++ compiler/src/CompilerInst.cpp
@@ -150,23 +150,37 @@ void CompilerInst::produceExecutable() {
         , &lld::SW_LLD_DRIVER_NAMESPACE::link
     };
 
-    // accumulates the linker arguments
-    std::vector args{
-        SW_LLD_DRIVER_NAME,  // the lld driver to invoke
-        (new std::string(runtime_path.string()))->c_str()  // the swirl runtime
-    };
+    // accumulate the runtime files
+    std::vector<std::string> sw_runtime{3};
+    sw_runtime.push_back(runtime_path.string());
+    if (triple.getOS() == llvm::Triple::Win32) {
+        sw_runtime.emplace_back("kernel32.lib");
+        sw_runtime.emplace_back("shell32.lib");
+        sw_runtime.emplace_back("/subsystem:console");
+        sw_runtime.emplace_back("/entry:_start");
+        sw_runtime.emplace_back("/STACK:4194304");
+        
+    }
+
+    // accumulate the linker arguments
+    std::vector args{SW_LLD_DRIVER_NAME};
+
+    // push all the runtime files
+    for (auto& arg : sw_runtime) {
+        args.push_back(arg.c_str());
+    }
 
     // iterate over all object files of the build directory and push their paths to the vector
     for (const auto& file : fs::directory_iterator(build_dir / "obj")) {
         args.push_back((new std::string(file.path().string()))->c_str());
-    } args.push_back("-o");
+    } args.push_back(triple.getOS() == llvm::Triple::Win32 ? "/OUT:" : "-o");
 
     // compute the output path
     if (m_OutputPath.empty())
         m_OutputPath = m_SrcPath.parent_path() / ".build" / (m_SrcPath
             .filename()
             .replace_extension()
-            .string() + ".out"
+            .string() + (triple.getOS() == llvm::Triple::Win32 ? ".exe" : ".out")
             );
 
     // push the output path to the vector
