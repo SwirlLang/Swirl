9e5f9c044b4e1f0db7e2f715c07fb726cc065821
nullptr <indiaonic@gmail.com>
2025-10-22 22:30:42 +0530

    fix: add missing `setBaseThreadCount` call

--- compiler/include/CompilerInst.h
+++ compiler/include/CompilerInst.h
@@ -57,6 +57,7 @@ public:
 
     void setBaseThreadCount(const std::string& count) {
         m_BaseThreadCount = static_cast<unsigned>(std::stoi(count));
+        m_ThreadPool.setBaseThreadCount(m_BaseThreadCount);
     }
 
     void setErrorPipeline(ErrorPipeline* pipeline) {
