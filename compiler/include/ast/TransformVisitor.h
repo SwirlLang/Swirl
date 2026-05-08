81b216571c6253c6214ceb26c506f4cee8b38deb
nullptr <indiaonic@gmail.com>
2026-05-09 00:13:10 +0530

    feat: add `TransformVisitor`

--- compiler/include/ast/TransformVisitor.h
+++ compiler/include/ast/TransformVisitor.h
@@ -1,12 +1,15 @@
 #pragma once
+#include "definitions.h"
 #include "Visitor.h"
+#include "modules/Module.h"
+
 
 struct Module;
 
 namespace detail {
 template <typename D, typename N, typename... Args>
 concept HasTransform = requires (D& derived, const N* node, Args&&... args) {
-    { derived->transform(node, std::forward<Args>(args)...) } -> std::convertible_to<N*>;
+    { derived.transform(node, std::forward<Args>(args)...) } -> std::convertible_to<N*>;
 };
 }
 
@@ -22,8 +25,8 @@ public:
     Node* run(const Node* node, Args&&... args) {
     #define SW_NODE(x, y) case x: \
         if constexpr (detail::HasTransform<Derived, y, Args...>) \
-            return transform(static_cast<y*>(node), std::forward<Args>(args)...); \
-        return transformDefault(static_cast<y*>(node), std::forward<Args>(args)...);
+            return transform(static_cast<const y*>(node), std::forward<Args>(args)...); \
+        return transformDefault(static_cast<const y*>(node), std::forward<Args>(args)...);
 
         switch (node->kind) {
             SW_NODE_LIST
@@ -32,12 +35,489 @@ public:
     }
 
 
-private:
-    Module* m_Module;
+protected:
+    template <typename T, typename... Args>
+    T* makeNode(Args&&... args) {
+        return m_Module->makeNode<T>(std::forward<Args>(args)...);
+    }
+
+
+    /// For leaf nodes
+    template <typename... Args>
+    const Node* transformDefault(const Node* node, Args&&... args) {
+        return node;
+    }
+
+
+    // --- default transform for nodes which hold pointers to other nodes --- //
 
     template <typename... Args>
-    Node* transformDefault(const Expression* node, Args&&... args) {
-        auto expr = run(node->expr);
-
+    const Node* transformDefault(const Expression* node, Args&&... args) {
+        auto expr = run(node->expr, std::forward<Args>(args)...);
+        if (expr != node->expr) {
+            auto new_node = makeNode<Expression>(*node);
+            new_node->expr = expr;
+            return new_node;
+        }
+        return node;
     }
+
+
+    template <typename... Args>
+    const Node* transformDefault(const Scope* node, Args&&... args) {
+        bool changed = false;
+        std::vector<Node*> new_children;
+        for (auto child : node->children) {
+            auto new_child = run(child, std::forward<Args>(args)...);
+            if (new_child != child) changed = true;
+            new_children.push_back(new_child);
+        }
+        if (changed) {
+            auto new_node = makeNode<Scope>(*node);
+            new_node->children = m_Module->internArray<Node*>(new_children);
+            return new_node;
+        }
+        return node;
+    }
+
+
+    template <typename... Args>
+    const Node* transformDefault(const FuncCall* node, Args&&... args) {
+        bool changed = false;
+
+        auto ident = run(node->ident, std::forward<Args>(args)...);
+        if (ident != node->ident) changed = true;
+
+        std::vector<Expression*> new_args;
+        for (auto arg : node->args) {
+            auto new_arg = run(arg, std::forward<Args>(args)...);
+            if (new_arg != arg) changed = true;
+            new_args.push_back(static_cast<Expression*>(new_arg));
+        }
+
+        auto gen_args = run(&node->generic_args, std::forward<Args>(args)...);
+        if (gen_args != &node->generic_args) changed = true;
+
+        if (changed) {
+            auto new_node = makeNode<FuncCall>(*node);
+            new_node->ident = static_cast<Ident*>(ident);
+            new_node->args = m_Module->internArray<Expression*>(new_args);
+            if (gen_args != &node->generic_args)
+                new_node->generic_args = *static_cast<GenericArgList*>(gen_args);
+            return new_node;
+        }
+        return node;
+    }
+
+
+    template <typename... Args>
+    const Node* transformDefault(const Function* node, Args&&... args) {
+        bool changed = false;
+
+        std::vector<GenericParam*> new_gen_params;
+        for (auto param : node->generic_params) {
+            auto new_param = run(param, std::forward<Args>(args)...);
+            if (new_param != param) changed = true;
+            new_gen_params.push_back(static_cast<GenericParam*>(new_param));
+        }
+
+        std::vector<Var*> new_params;
+        for (auto param : node->params) {
+            auto new_param = run(param, std::forward<Args>(args)...);
+            if (new_param != param) changed = true;
+            new_params.push_back(static_cast<Var*>(new_param));
+        }
+
+        if (node->return_type) {
+            auto ret = run(node->return_type, std::forward<Args>(args)...);
+            if (ret != node->return_type) changed = true;
+        }
+
+        std::vector<Node*> new_children;
+        for (auto child : node->children) {
+            auto new_child = run(child, std::forward<Args>(args)...);
+            if (new_child != child) changed = true;
+            new_children.push_back(new_child);
+        }
+
+        if (changed) {
+            auto new_node = makeNode<Function>(*node);
+            new_node->generic_params = m_Module->internArray<GenericParam*>(new_gen_params);
+            new_node->params = m_Module->internArray<Var*>(new_params);
+            new_node->children = m_Module->internArray<Node*>(new_children);
+            return new_node;
+        }
+        return node;
+    }
+
+
+    template <typename... Args>
+    const Node* transformDefault(const Condition* node, Args&&... args) {
+        bool changed = false;
+
+        auto bool_expr = run(node->bool_expr, std::forward<Args>(args)...);
+        if (bool_expr != node->bool_expr) changed = true;
+
+        std::vector<Node*> new_if_children;
+        for (auto child : node->if_children) {
+            auto new_child = run(child, std::forward<Args>(args)...);
+            if (new_child != child) changed = true;
+            new_if_children.push_back(new_child);
+        }
+
+        std::vector<Condition::elif_t> new_elif;
+        for (auto& [expr, body] : node->elif_children) {
+            auto new_expr = run(expr, std::forward<Args>(args)...);
+            bool expr_changed = (new_expr != expr);
+
+            bool body_changed = false;
+            std::vector<Node*> new_body;
+            for (auto child : body) {
+                auto new_child = run(child, std::forward<Args>(args)...);
+                if (new_child != child) body_changed = true;
+                new_body.push_back(new_child);
+            }
+
+            if (expr_changed || body_changed) {
+                changed = true;
+                auto interned_body = m_Module->internArray<Node*>(new_body);
+                new_elif.emplace_back(static_cast<Expression*>(new_expr), interned_body);
+            } else {
+                new_elif.emplace_back(static_cast<Expression*>(new_expr), body);
+            }
+        }
+
+        std::vector<Node*> new_else_children;
+        for (auto child : node->else_children) {
+            auto new_child = run(child, std::forward<Args>(args)...);
+            if (new_child != child) changed = true;
+            new_else_children.push_back(new_child);
+        }
+
+        if (changed) {
+            auto new_node = makeNode<Condition>(*node);
+            new_node->bool_expr = static_cast<Expression*>(bool_expr);
+            new_node->if_children = m_Module->internArray<Node*>(new_if_children);
+            new_node->elif_children = m_Module->internArray<Condition::elif_t>(new_elif);
+            new_node->else_children = m_Module->internArray<Node*>(new_else_children);
+            return new_node;
+        }
+        return node;
+    }
+
+
+    template <typename... Args>
+    const Node* transformDefault(const Struct* node, Args&&... args) {
+        bool changed = false;
+
+        std::vector<GenericParam*> new_gen_params;
+        for (auto param : node->generic_params) {
+            auto new_param = run(param, std::forward<Args>(args)...);
+            if (new_param != param) changed = true;
+            new_gen_params.push_back(static_cast<GenericParam*>(new_param));
+        }
+
+        std::vector<Ident*> new_protocols;
+        for (auto proto : node->protocols) {
+            auto new_proto = run(proto, std::forward<Args>(args)...);
+            if (new_proto != proto) changed = true;
+            new_protocols.push_back(static_cast<Ident*>(new_proto));
+        }
+
+        std::vector<Node*> new_members;
+        for (auto member : node->members) {
+            auto new_member = run(member, std::forward<Args>(args)...);
+            if (new_member != member) changed = true;
+            new_members.push_back(new_member);
+        }
+
+        if (changed) {
+            auto new_node = makeNode<Struct>(*node);
+            new_node->generic_params = m_Module->internArray<GenericParam*>(new_gen_params);
+            new_node->protocols = m_Module->internArray<Ident*>(new_protocols);
+            new_node->members = m_Module->internArray<Node*>(new_members);
+            return new_node;
+        }
+        return node;
+    }
+
+
+    template <typename... Args>
+    const Node* transformDefault(const ArrayLit* node, Args&&... args) {
+        bool changed = false;
+        std::vector<Expression*> new_elements;
+        for (auto elem : node->elements) {
+            auto new_elem = run(elem, std::forward<Args>(args)...);
+            if (new_elem != elem) changed = true;
+            new_elements.push_back(static_cast<Expression*>(new_elem));
+        }
+        if (changed) {
+            auto new_node = makeNode<ArrayLit>(*node);
+            new_node->elements = m_Module->internArray<Expression*>(new_elements);
+            return new_node;
+        }
+        return node;
+    }
+
+
+    template <typename... Args>
+    const Node* transformDefault(const TypeWrapper* node, Args&&... args) {
+        bool changed = false;
+
+        auto type_id = run(node->type_id, std::forward<Args>(args)...);
+        if (type_id != node->type_id) changed = true;
+
+        if (node->of_type) {
+            auto of_type = run(node->of_type, std::forward<Args>(args)...);
+            if (of_type != node->of_type) changed = true;
+        }
+
+        if (changed) {
+            auto new_node = makeNode<TypeWrapper>(*node);
+            if (type_id != node->type_id)
+                new_node->type_id = static_cast<Ident*>(type_id);
+            return new_node;
+        }
+        return node;
+    }
+
+
+    template <typename... Args>
+    const Node* transformDefault(const Var* node, Args&&... args) {
+        bool changed = false;
+
+        if (node->var_type) {
+            auto var_type = run(node->var_type, std::forward<Args>(args)...);
+            if (var_type != node->var_type) changed = true;
+        }
+
+        if (node->initialized) {
+            auto value = run(node->value, std::forward<Args>(args)...);
+            if (value != node->value) changed = true;
+        }
+
+        if (changed) {
+            auto new_node = makeNode<Var>(*node);
+            return new_node;
+        }
+        return node;
+    }
+
+
+    template <typename... Args>
+    const Node* transformDefault(const Op* node, Args&&... args) {
+        bool changed = false;
+        std::vector<Node*> new_operands;
+        for (auto operand : node->operands) {
+            auto new_operand = run(operand, std::forward<Args>(args)...);
+            if (new_operand != operand) changed = true;
+            new_operands.push_back(new_operand);
+        }
+        if (changed) {
+            auto new_node = makeNode<Op>(*node);
+            new_node->operands = m_Module->internArray<Node*>(new_operands);
+            return new_node;
+        }
+        return node;
+    }
+
+
+    template <typename... Args>
+    const Node* transformDefault(const Intrinsic* node, Args&&... args) {
+        bool changed = false;
+
+        auto ident = run(node->ident, std::forward<Args>(args)...);
+        if (ident != node->ident) changed = true;
+
+        std::vector<Expression*> new_args;
+        for (auto arg : node->args) {
+            auto new_arg = run(arg, std::forward<Args>(args)...);
+            if (new_arg != arg) changed = true;
+            new_args.push_back(static_cast<Expression*>(new_arg));
+        }
+
+        if (changed) {
+            auto new_node = makeNode<Intrinsic>(*node);
+            new_node->ident = static_cast<Ident*>(ident);
+            new_node->args = m_Module->internArray<Expression*>(new_args);
+            return new_node;
+        }
+        return node;
+    }
+
+
+    template <typename... Args>
+    const Node* transformDefault(const Protocol* node, Args&&... args) {
+        bool changed = false;
+
+        for (auto dep : node->depended_protocols) {
+            auto new_dep = run(dep, std::forward<Args>(args)...);
+            if (new_dep != dep) changed = true;
+        }
+
+        std::vector<Protocol::MemberSignature> new_members;
+        for (auto& member : node->members) {
+            auto new_type = run(member.type, std::forward<Args>(args)...);
+            if (new_type != member.type) {
+                changed = true;
+                auto m = member;
+                m.type = static_cast<TypeWrapper*>(new_type);
+                new_members.push_back(m);
+            } else {
+                new_members.push_back(member);
+            }
+        }
+
+        std::vector<Protocol::MethodSignature> new_methods;
+        for (auto& method : node->methods) {
+            bool method_changed = false;
+
+            auto new_ret = run(method.return_type, std::forward<Args>(args)...);
+            if (new_ret != method.return_type) method_changed = true;
+
+            std::vector<TypeWrapper*> new_params;
+            for (auto param : method.params) {
+                auto new_param = run(param, std::forward<Args>(args)...);
+                if (new_param != param) method_changed = true;
+                new_params.push_back(static_cast<TypeWrapper*>(new_param));
+            }
+
+            if (method_changed) {
+                changed = true;
+                auto m = method;
+                m.return_type = static_cast<TypeWrapper*>(new_ret);
+                m.params = m_Module->internArray<TypeWrapper*>(new_params);
+                new_methods.push_back(m);
+            } else {
+                new_methods.push_back(method);
+            }
+        }
+
+        if (changed) {
+            auto new_node = makeNode<Protocol>(*node);
+            new_node->members = m_Module->internArray<Protocol::MemberSignature>(new_members);
+            new_node->methods = m_Module->internArray<Protocol::MethodSignature>(new_methods);
+            return new_node;
+        }
+        return node;
+    }
+
+
+    template <typename... Args>
+    const Node* transformDefault(const WhileLoop* node, Args&&... args) {
+        bool changed = false;
+
+        auto condition = run(node->condition, std::forward<Args>(args)...);
+        if (condition != node->condition) changed = true;
+
+        std::vector<Node*> new_children;
+        for (auto child : node->children) {
+            auto new_child = run(child, std::forward<Args>(args)...);
+            if (new_child != child) changed = true;
+            new_children.push_back(new_child);
+        }
+
+        if (changed) {
+            auto new_node = makeNode<WhileLoop>(*node);
+            new_node->condition = static_cast<Expression*>(condition);
+            new_node->children = m_Module->internArray<Node*>(new_children);
+            return new_node;
+        }
+        return node;
+    }
+
+
+    template <typename... Args>
+    const Node* transformDefault(const Enum* node, Args&&... args) {
+        if (node->enum_type.has_value()) {
+            auto new_type = run(node->enum_type.value(), std::forward<Args>(args)...);
+            if (new_type != node->enum_type.value()) {
+                auto new_node = makeNode<Enum>(*node);
+                new_node->enum_type = static_cast<TypeWrapper*>(new_type);
+                return new_node;
+            }
+        }
+        return node;
+    }
+
+
+    template <typename... Args>
+    const Node* transformDefault(const ReturnStatement* node, Args&&... args) {
+        if (node->value) {
+            auto value = run(node->value, std::forward<Args>(args)...);
+            if (value != node->value) {
+                auto new_node = makeNode<ReturnStatement>(*node);
+                new_node->value = static_cast<Expression*>(value);
+                return new_node;
+            }
+        }
+        return node;
+    }
+
+
+    template <typename... Args>
+    const Node* transformDefault(const Ident* node, Args&&... args) {
+        bool changed = false;
+        std::vector<Ident::Qualifier> new_qual;
+        for (auto& q : node->full_qualification) {
+            auto gen_args = run(&q.generic_args, std::forward<Args>(args)...);
+            if (gen_args != &q.generic_args) {
+                changed = true;
+                auto q2 = q;
+                q2.generic_args = *static_cast<GenericArgList*>(gen_args);
+                new_qual.push_back(q2);
+            } else {
+                new_qual.push_back(q);
+            }
+        }
+        if (changed) {
+            auto new_node = makeNode<Ident>(*node);
+            new_node->full_qualification = m_Module->internArray<Ident::Qualifier>(new_qual);
+            return new_node;
+        }
+        return node;
+    }
+
+
+    template <typename... Args>
+    const Node* transformDefault(const GenericArgList* node, Args&&... args) {
+        bool changed = false;
+        std::vector<GenericArg*> new_args;
+        for (auto arg : node->generic_args) {
+            auto new_arg = run(arg, std::forward<Args>(args)...);
+            if (new_arg != arg) changed = true;
+            new_args.push_back(static_cast<GenericArg*>(new_arg));
+        }
+        if (changed) {
+            auto new_node = makeNode<GenericArgList>(*node);
+            new_node->generic_args = m_Module->internArray<GenericArg*>(new_args);
+            return new_node;
+        }
+        return node;
+    }
+
+
+    template <typename... Args>
+    const Node* transformDefault(const GenericArg* node, Args&&... args) {
+        if (node->isExpression()) {
+            auto expr = run(node->getExpr(), std::forward<Args>(args)...);
+            if (expr != node->getExpr()) {
+                const auto new_node = makeNode<GenericArg>(
+                    static_cast<Expression*>(expr));
+                return new_node;
+            }
+        } else if (node->isType()) {
+            auto type = run(node->getType(), std::forward<Args>(args)...);
+            if (type != node->getType()) {
+                const auto new_node = makeNode<GenericArg>(
+                    static_cast<TypeWrapper*>(type));
+                return new_node;
+            }
+        }
+        return node;
+    }
+
+
+private:
+    Module* m_Module;
 };
